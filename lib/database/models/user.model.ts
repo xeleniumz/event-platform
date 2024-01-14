7c71b7b3446cc0c773e92d8f10115dc612316923
xeleniumz <demolismz@windowslive.com>
2024-01-14 16:08:59 +0700

    add actions & db

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema } from "mongoose";
+import { model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: false,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
